d2ab7695e788069c6603baee59a9796c7b6afb17
Gaston Lions <glions@f4r13s8.clusters.42paris.fr>
2026-01-23 11:32:34 +0100

    training finit

--- training.cpp
+++ training.cpp
@@ -6,7 +6,7 @@
 /*   By: glions <glions@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 11:35:39 by glions            #+#    #+#             */
-/*   Updated: 2026/01/22 16:19:47 by glions           ###   ########.fr       */
+/*   Updated: 2026/01/23 11:31:14 by glions           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -413,110 +413,6 @@ void	exo8()
 	}
 }
 
-void	exo8()
-{
-	struct State
-	{
-		int	value;
-
-		bool operator==(const State &other) const
-		{
-			return (value == other.value);
-		}
-
-		bool operator!=(const State &other) const
-		{
-			return (value != other.value);
-		}
-	};
-
-	struct StateHash
-	{
-		std::size_t operator()(const State &s) const
-		{
-			return (std::hash<int>()(s.value));
-		}
-	};
-
-	struct	Elem
-	{
-		int		distance;
-		int		estimate;
-		State 	state;
-	};
-
-	struct Cmp
-	{
-		bool operator()(const Elem &a, const Elem &b) const
-		{
-			return (a.distance + a.estimate > b.distance + b.estimate);
-		}
-	};
-
-	std::unordered_map<State, std::vector<std::pair<State, int>>, StateHash> graph;
-	graph[State{1}] = {{State{2}, 2}, {State{3}, 5}, {State{4}, 1}};
-	graph[State{2}] = {{State{1}, 2}, {State{5}, 2}, {State{6}, 4}};
-	graph[State{3}] = {{State{1}, 5}, {State{6}, 1}};
-	graph[State{4}] = {{State{1}, 1}, {State{6}, 7}};
-	graph[State{5}] = {{State{2}, 2}, {State{7}, 3}};
-	graph[State{6}] = {{State{2}, 4}, {State{3}, 1}, {State{4}, 7}, {State{7}, 2}};
-	graph[State{7}] = {{State{5}, 3}, {State{6}, 2}, {State{8}, 1}};
-	graph[State{8}] = {{State{7}, 1}};
-	
-	std::unordered_map<State, int, StateHash> distances;
-	std::unordered_map<State, State, StateHash> fathers;
-	std::priority_queue<Elem, std::vector<Elem>, Cmp> tmp;
-
-	State start = State{1};
-	distances[start] = 0;
-	State dest = State{8};
-	tmp.push(Elem{.distance= 0, .estimate=dest.value - start.value,.state= start});
-	while (!tmp.empty())
-	{
-		Elem val = tmp.top();
-		tmp.pop();
-		if (val.state == dest)
-		{
-			std::cout << "Dest atteind donc on stop" << std::endl;
-			break;
-		}
-		if (val.distance > distances[val.state])
-		{
-			std::cout << "Distance connue plus petite donc on ignore" << std::endl;
-			continue;
-		}
-		std::cout << "Traitement de " << val.state.value << std::endl;
-		auto neigh = graph.find(val.state);
-		if (neigh == graph.end())
-			std::cout << "Ne possede pas de voisins" << std::endl;
-		else
-		{
-			for (auto n : neigh->second)
-			{
-				int d = distances[val.state] + n.second;
-				if ((distances.find(n.first) != distances.end()
-					&& distances.find(n.first)->second > d)
-					|| distances.find(n.first) == distances.end())
-				{
-					distances[n.first] = d;
-					fathers[n.first] = val.state;
-					tmp.push(Elem({.distance= distances[n.first], .estimate=dest.value - n.first.value,.state= n.first}));
-					std::cout << "Voisin1 -> " << n.first.value << " distance -> ";
-					std::cout << distances[n.first] << " parent -> " << fathers[n.first].value << std::endl;
-				}
-			}
-		}
-	}
-	std::cout << "Distance entre " << start.value << " et " << dest.value << " -> " << distances[dest] << std::endl;
-	std::cout << "Parcours entre les deux points : " << std::endl;
-	std::cout << dest.value << std::endl;
-	while (dest != start)
-	{
-		dest = State{fathers[dest]};
-		std::cout << dest.value << std::endl;
-	}
-}
-
 int		calc(int y, int x, int v, std::array<std::array<int, 3>, 3> &b)
 {
 	for (size_t i = 0; i < b.size(); i++)
@@ -524,7 +420,7 @@ int		calc(int y, int x, int v, std::array<std::array<int, 3>, 3> &b)
 		for(size_t j = 0; j < b[i].size(); j++)
 		{
 			if (b[i][j] == v)
-				return (abs(y - i) + abs(x - j));
+				return (abs(y - (int)i) + abs(x - (int)j));
 		}
 	}
 	return (0);
@@ -621,8 +517,6 @@ void	exo9()
 		{7, 8, 0} 
 	}}};
 
-	
-
 	std::unordered_map<State, int, StateHash> distances;
 	std::unordered_map<State, State, StateHash> fathers;
 	std::priority_queue<Elem, std::vector<Elem>, Cmp> tmp;
@@ -667,15 +561,13 @@ void	exo9()
 				copy[pos.y][pos.x - 1] = arrTmp;
 				State newState = State{copy};
 				int d = distances[val.state] + 1;
-				if ((distances.find(n.first) != distances.end()
-					&& distances.find(n.first)->second > d)
-					|| distances.find(n.first) == distances.end())
+				if ((distances.find(newState) != distances.end()
+					&& distances.find(newState)->second > d)
+					|| distances.find(newState) == distances.end())
 				{
-					distances[n.first] = d;
-					fathers[n.first] = val.state;
-					tmp.push(Elem({.distance= distances[n.first], .estimate=dest.value - n.first.value,.state= n.first}));
-					std::cout << "Voisin1 -> " << n.first.value << " distance -> ";
-					std::cout << distances[n.first] << " parent -> " << fathers[n.first].value << std::endl;
+					distances[newState] = d;
+					fathers[newState] = val.state;
+					tmp.push(Elem({.distance= distances[newState], .estimate=calcEstimation(dest.values, newState.values),.state= newState}));
 				}
 			}
 			if (pos.x < val.state.values.size() - 1)
@@ -686,6 +578,15 @@ void	exo9()
 				copy[pos.y][pos.x] = copy[pos.y][pos.x + 1];
 				copy[pos.y][pos.x + 1] = arrTmp;
 				State newState = State{copy};
+				int d = distances[val.state] + 1;
+				if ((distances.find(newState) != distances.end()
+					&& distances.find(newState)->second > d)
+					|| distances.find(newState) == distances.end())
+				{
+					distances[newState] = d;
+					fathers[newState] = val.state;
+					tmp.push(Elem({.distance= distances[newState], .estimate=calcEstimation(dest.values, newState.values),.state= newState}));
+				}
 			}
 			if (pos.y > 0)
 			{
@@ -695,6 +596,15 @@ void	exo9()
 				copy[pos.y][pos.x] = copy[pos.y - 1][pos.x];
 				copy[pos.y - 1][pos.x] = arrTmp;
 				State newState = State{copy};
+				int d = distances[val.state] + 1;
+				if ((distances.find(newState) != distances.end()
+					&& distances.find(newState)->second > d)
+					|| distances.find(newState) == distances.end())
+				{
+					distances[newState] = d;
+					fathers[newState] = val.state;
+					tmp.push(Elem({.distance= distances[newState], .estimate=calcEstimation(dest.values, newState.values),.state= newState}));
+				}
 			}
 			if (pos.y < val.state.values.size() - 1)
 			{
@@ -704,36 +614,63 @@ void	exo9()
 				copy[pos.y][pos.x] = copy[pos.y + 1][pos.x];
 				copy[pos.y + 1][pos.x] = arrTmp;
 				State newState = State{copy};
+				int d = distances[val.state] + 1;
+				if ((distances.find(newState) != distances.end()
+					&& distances.find(newState)->second > d)
+					|| distances.find(newState) == distances.end())
+				{
+					distances[newState] = d;
+					fathers[newState] = val.state;
+					tmp.push(Elem({.distance= distances[newState], .estimate=calcEstimation(dest.values, newState.values),.state= newState}));
+				}
 			}
 		}
-	// 	auto neigh = graph.find(val.state);
-	// 	if (neigh == graph.end())
-	// 		std::cout << "Ne possede pas de voisins" << std::endl;
-	// 	else
-	// 	{
-	// 		for (auto n : neigh->second)
-	// 		{
-	// 			int d = distances[val.state] + n.second;
-	// 			if ((distances.find(n.first) != distances.end()
-	// 				&& distances.find(n.first)->second > d)
-	// 				|| distances.find(n.first) == distances.end())
-	// 			{
-	// 				distances[n.first] = d;
-	// 				fathers[n.first] = val.state;
-	// 				tmp.push(Elem({.distance= distances[n.first], .estimate=dest.value - n.first.value,.state= n.first}));
-	// 				std::cout << "Voisin1 -> " << n.first.value << " distance -> ";
-	// 				std::cout << distances[n.first] << " parent -> " << fathers[n.first].value << std::endl;
-	// 			}
-	// 		}
-	// 	}
-	// }
-	// std::cout << "Distance entre " << start.value << " et " << dest.value << " -> " << distances[dest] << std::endl;
-	// std::cout << "Parcours entre les deux points : " << std::endl;
-	// std::cout << dest.value << std::endl;
-	// while (dest != start)
-	// {
-	// 	dest = State{fathers[dest]};
-	// 	std::cout << dest.value << std::endl;
+	}
+	std::cout << "Start : " << std::endl;
+	for (size_t i = 0; i < start.values.size(); i++)
+	{
+		for (size_t j = 0; j < start.values[i].size(); j++)
+		{
+			std::cout << " " << start.values[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "Dest : " << std::endl;
+	for (size_t i = 0; i < dest.values.size(); i++)
+	{
+		for (size_t j = 0; j < dest.values[i].size(); j++)
+		{
+			std::cout << " " << dest.values[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "Distance : " << distances[dest] << std::endl;
+	std::cout << "Chemin parcouru : " << std::endl;
+	for (size_t i = 0; i < dest.values.size(); i++)
+	{
+		for (size_t j = 0; j < dest.values[i].size(); j++)
+		{
+			std::cout << " " << dest.values[i][j];
+		}
+		std::cout << std::endl;
+	}
+	int i = 0;
+	std::cout << std::endl;
+	while (dest != start)
+	{
+		dest = State{fathers[dest]};
+		for (size_t i = 0; i < dest.values.size(); i++)
+		{
+			for (size_t j = 0; j < dest.values[i].size(); j++)
+			{
+				std::cout << " " << dest.values[i][j];
+			}
+			std::cout << std::endl;
+		}
+		i++;
+		std::cout << std::endl;
+		std::cout << i << std::endl;
+		std::cout << std::endl;
 	}
 }
 
@@ -745,10 +682,11 @@ int	main()
 	// exo4();
 	// exo5();
 	// exo6();
-	std::cout << "SANS A*" << std::endl;
-	exo7();
-	std::cout << std::endl << std::endl << std::endl;
-	std::cout << "AVEC A*" << std::endl;
-	exo8();
+	// std::cout << "SANS A*" << std::endl;
+	// exo7();
+	// std::cout << std::endl << std::endl << std::endl;
+	// std::cout << "AVEC A*" << std::endl;
+	// exo8();
+	exo9();
 	return (0);
 }
